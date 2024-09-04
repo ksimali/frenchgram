d6a4dd738ab8b7f39620c0a671887665b0901456
ksimali <cassim.ali@outlook.fr>
2024-09-04 13:29:51 -0400

    Add LoginComponene in app.module and Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+
+import { MatCardModule} from '@angular/material/card'
 import { MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule} from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, // Required for Angular Material
     AppRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
     MatInputModule,
     MatFormFieldModule,
     MatIconModule,
-    FormsModule
-
+    MatButtonModule,
   ],
   providers: [
     provideClientHydration(),
