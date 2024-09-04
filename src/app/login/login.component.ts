dbb100b6f1982ccd6c792aceb16edd07179d9677
ksimali <cassim.ali@outlook.fr>
2024-09-04 11:42:39 -0400

    Setup LoginComponent

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  constructor(private userService: UserService){
+
+  }
+}
