1016f28ec30d7e5abf4bf63c2857e30fe14bde5a
ksimali <cassim.ali@outlook.fr>
2024-09-04 16:39:25 -0400

    Update comment in user.service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  //method login
+  // login() method
   public login(email:string, password: string): Observable<any>{
     return this.http.post(`${this.apiUrl}/login`,{email, password});
   }
