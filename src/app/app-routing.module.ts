29ffc1ebd4dc820a4237232b04ae97a7fa98aaa7
ksimali <cassim.ali@outlook.fr>
2024-09-05 01:50:46 -0400

    Add path for signup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path:'', component: SearchComponent},
-  {path:'login', component:LoginComponent}
+  {path:'', component:LoginComponent},
+  {path:'search', component: SearchComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent}
 ];
 
 @NgModule({
