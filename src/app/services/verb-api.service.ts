9468fa9a622a9a6541d0d12d771af55e6c3e137d
ksimali <cassim.ali@outlook.fr>
2024-09-05 00:20:03 -0400

    Setup verb-api.model & verb-api.service

--- src/app/services/verb-api.service.ts
+++ src/app/services/verb-api.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,12 @@ export class VerbApiService {
   private apiUrl = 'https://seal-app-v5cj7.ondigitalocean.app/v0/verbs';
 
   constructor(private http: HttpClient) { }
+
+  getVerb(token: string): Observable<any>{
+    const headers = new HttpHeaders({
+      'x-access-token': token
+    });
+
+    return this.http.get(`${this.apiUrl}`,{headers});
+  }
 }
