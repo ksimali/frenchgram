b9b7ff961b7e601f8082e772cda88543cbb4dc35
ksimali <cassim.ali@outlook.fr>
2024-09-05 01:53:07 -0400

    Setup signup component ts file and template +scss

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  signupForm: FormGroup;
+
+  constructor(private userService: UserService, private fb: FormBuilder){
+    // creation du form
+    this.signupForm = this.fb.group({
+      email: ['', Validators.required],
+      name:['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSignup(){
+    if(this.signupForm.valid){
+      //recuperer les infos du form
+      const{email, name, password} = this.signupForm.value;
+
+      this.userService.signup(email, name, password).subscribe(
+        (res)=> {
+          alert(`${name} a bien été inscrit avec succès!`);
+          console.log(res);
+        },
+        (err) => {
+          alert("L'inscription à échoué!");
+          console.log(err);
+        }
+      );
+    } 
+  }
 
 }
