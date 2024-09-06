917112826dedf8d2ba4b7177b5e0e36af9e6db7b
ksimali <cassim.ali@outlook.fr>
2024-09-06 15:07:31 -0400

    Setup VerbApi model

--- src/app/models/verb-api.model.ts
+++ src/app/models/verb-api.model.ts
@@ -1,3 +1,18 @@
 export interface VerbApi{
-    
+    conditionnel: JSON,
+    fullDescription:string,
+    imperatif: JSON,
+    indicatif: JSON,
+    infinitive: JSON,
+    participe: JSON,
+    subjonctif: JSON,
+    verb: string,
+    word: string,
+    wordConjugateRule: string,
+    wordConjugateWhichVerbModel: string,
+    wordConjugateWithWhichVerb: string,
+    wordVerbGroup: string,
+    wordVerbType: string
+
+
 }
