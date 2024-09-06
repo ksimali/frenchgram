c47e94e27f0aa5f53c17bcb45a4a2351d823db41
ksimali <cassim.ali@outlook.fr>
2024-09-06 12:09:30 -0400

    Update verb-conjugation.component: - Add propreties, searchForm constructor and method -Create a serchForm in VerbConjugationTemplate and Bind it to the form in VerbConjugation class - Add scss style

--- src/app/verb-conjugation/verb-conjugation.component.ts
+++ src/app/verb-conjugation/verb-conjugation.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { VerbApi } from '../models/verb-api.model';
 import { VerbApiService } from '../services/verb-api.service';
 import { Router } from 'express';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-verb-conjugation',
@@ -10,8 +11,30 @@ import { Router } from 'express';
 })
 export class VerbConjugationComponent {
 
-  constructor(private verbApiService: VerbApiService){
+  searchForm:FormGroup;
 
+  constructor(private verbApiService : VerbApiService, private fb: FormBuilder ){
+    this.searchForm = this.fb.group({
+      search: [''],
+    });
+   }
+
+  onSearch(){
+    if(this.searchForm.valid){
+      //recuperer les infos du form
+      const search = this.searchForm.value.search; // recuper la value direct et non l'objet
+      console.log(search);
+      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhc3NpbUBnbWFpbC5jb20iLCJ1aWQiOiI2NmQ4NzNlOThhZjNlNmQ5OWMyNmJhNTQiLCJleHAiOjE3MjgwNTg3OTB9.sm0L5C404aY-mb0yfUJlHAGRfyOjRNo9N9vK4WO1bOM";
+
+      this.verbApiService.getVerb(token, search).subscribe(
+        (res)=> {
+          console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
   }
 
 }
