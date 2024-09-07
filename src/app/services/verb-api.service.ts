4432a70fb6ea16fd72c8d8f4169c1ef39ac51d00
ksimali <cassim.ali@outlook.fr>
2024-09-07 14:11:38 -0400

    Update verb-api.service to get token in session storage : - Add AuthService to the constructor - Modify x-access-token line17

--- src/app/services/verb-api.service.ts
+++ src/app/services/verb-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,12 @@ import { Observable } from 'rxjs';
 export class VerbApiService {
   private apiUrl:string = 'https://seal-app-v5cj7.ondigitalocean.app/v0/verbs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   getVerb(token: string, verb: string): Observable<any>{
     // defintion du header
     const headers = new HttpHeaders({
-      'x-access-token': token
+      'x-access-token': this.authService.getToken() || "",
     });
     // defintion du body
     const body = {
