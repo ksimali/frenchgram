5c8827859b8d4871d44a641f876e12c4cf020941
ksimali <cassim.ali@outlook.fr>
2024-09-07 18:58:27 -0400

    Update Routes: - Add SearchComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { VerbConjugationComponent } from './verb-conjugation/verb-conjugation.component';
 import { AuthGuard } from './guards/auth.guard';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
-  {path:'', component:VerbConjugationComponent, canActivate: [AuthGuard]},
+  {path:'', component:SearchComponent, canActivate: [AuthGuard]},
   {path:'login', component:LoginComponent},
   {path:'signup', component:SignupComponent},
-  {path:'verbconjugation', component:VerbConjugationComponent, canActivate: [AuthGuard]}
+  //{path:'verbconjugation', component:VerbConjugationComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
