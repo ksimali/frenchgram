3a04c521b43d9f4b45ea2fb6a9369ce50c775652
ksimali <cassim.ali@outlook.fr>
2024-09-07 01:20:39 -0400

    Update path in app-routine module: - Delete path for Search component -Update VerbConjugation path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { SignupComponent } from './signup/signup.component';
 import { VerbConjugationComponent } from './verb-conjugation/verb-conjugation.component';
 
 const routes: Routes = [
-  {path:'', component:LoginComponent},
-  {path:'search', component: SearchComponent},
+  {path:'', component:VerbConjugationComponent},
   {path:'login', component:LoginComponent},
   {path:'signup', component:SignupComponent},
   {path:'verbconjugation', component:VerbConjugationComponent}
